ce1efa5181cf3fcd6d6a47e7d58d30dc7fa35436
Patrick Kearns <kearnspj23@gmail.com>
2025-04-09 13:47:03 -0400

    Update r_analysis.R

--- r_analysis.R
+++ r_analysis.R
@@ -103,85 +103,16 @@ ggplot(alphy, aes(as.numeric(FusariumDose), `specnumber(cort_rare)`))+
   geom_smooth(method='lm')+
   theme_bw()
 
-#percent inhibitory analysis
-#calculate in vsearch first
-vsearch -usearch_global rep_seqs/dna-sequences.fasta -db antiBd_db/AmphibBac_Inhibitory_2023.2r.fasta --strand plus --id 0.99 --blast6out synth_com_inhib_otus.txt
-#Matching unique query sequences: 30 of 66 (45.45%)
+#run homogeneity of variances
+bartlett.test(cort.alph$`specnumber(cort_rare)` ~ cort.alph$Antibiotic)
+#Bartlett's K-squared = 8.8968, df = 7, p-value = 0.2602
 
-#read in results from vsearch clustering against AmphiBac database
-inhibitory<-read.delim("~/GitHub/Newt-cort-microbiome/Synth_com_data/synth_com_inhib_otus.txt", header=F)
-#ASVs are V1 in df
+bartlett.test(cort.alph$`specnumber(cort_rare)` ~ cort.alph$Fusarium)
+#Bartlett's K-squared = 0.54816, df = 1, p-value = 0.4591
 
-#load in asv table and metadata
-asv_table <- read.delim("~/GitHub/Newt-cort-microbiome/Synth_com_data/asv_table_synth_com.txt", row.names=1, header=T)
-meta<-read.delim("~/GitHub/Newt-cort-microbiome//Synth_com_data/cort_synth_com_map.txt", header=T)
-
-#rarefy data 
-set.seed(515)
-library(vegan)
-asv_table<-rrarefy(t(asv_table), sample=4591)
-asv_table<-as.data.frame(t(asv_table))
-
-#subset ASV table to only include ASVs that match database
-inhib_tb<-asv_table[row.names(asv_table) %in% inhibitory$V1,]
-
-#calculate inhibitory richness
-cort.alph<-as.data.frame(specnumber(t(inhib_tb)))
-cort.alph$SampleID<-row.names(cort.alph)
-cort.alph<-merge(cort.alph, meta, by='SampleID')
-
-#plot antifungal richness
-library(ggplot2)
-synth_com_antifungal_richness<-ggplot(cort.alph, aes(Type, `specnumber(t(inhib_tb))`, fill=Type))+
-  geom_jitter()+
-  geom_boxplot()+
-  scale_fill_manual(values=c('blue', 'grey'))+
-  theme_bw()+
-  ggtitle("(D)")+
-  xlab("")+
-  coord_flip()+
-  ylab("Antifungal richness")
-
-#math it
-t.test(cort.alph$`specnumber(t(inhib_tb))` ~ cort.alph$Type)
-# = -1.7821, df = 24.724, p-value = 0.08702
-
-#calculate colSums for total/inhibitory communities
-total_sum<-as.data.frame(colSums(asv_table))
-inhib_sum<-as.data.frame(colSums(inhib_tb))
-
-#bind data together
-inhib_tb2<-cbind(total_sum, inhib_sum)
-
-#calculate percent inhibitory
-inhib_tb2$per_inhib<-inhib_tb2$`colSums(inhib_tb)`/inhib_tb2$`colSums(asv_table)`
-
-#add column for SampleID and merge metadata
-inhib_tb2$SampleID<-row.names(inhib_tb2)
-inhib_tb2<-merge(inhib_tb2, meta, by='SampleID')
-inhib_tb2$per_inhib2<-inhib_tb2$per_inhib*100
-
-#plot per species
-library(ggplot2)
-synthcom_bd_per_inhib<-
-  ggplot(inhib_tb2, aes(Type, per_inhib2, fill=Type))+
-  geom_jitter()+
-  geom_boxplot()+
-  guides(fill="none")+
-  theme_bw()+
-  ggtitle("(C)")+
-  xlab("")+
-  coord_flip()+
-  scale_fill_manual(values = c('blue', 'grey'))+
-  ylab("Percent Inhibitory towards Bd")
-
-#test it with maths
-t.test(inhib_tb2$per_inhib ~ inhib_tb2$Type)
-#t = -1.4482, df = 27.617, p-value = 0.1588
-
-##Make publucation figure
-library(ggpubr)
-ggarrange(synth_com_pcoa, synth_com_richness, synthcom_bd_per_inhib, synth_com_antifungal_richness, legend = c('none','none', 'none', 'right'))
+#run AOV
+summary(aov(cort.alph$`specnumber(cort_rare)` ~ cort.alph$Antibiotic))
+TukeyHSD(aov(cort.alph$`specnumber(cort_rare)` ~ cort.alph$Antibiotic))
 
 
 ###Calculate ASVs that respond (+/-) to cort
